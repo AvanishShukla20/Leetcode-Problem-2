d23466fdbd0f2d78f221bc2a7fd78d4aebf8d82f
Avanish Shukla <avanishshukla2021@gmail.com>
2025-08-09 01:48:09 +0530

    Time: 28 ms (68.82%), Space: 29.8 MB (78.34%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int solve(int i, int j, string& s, vector<vector<int>>& dp)
     {
-        if(i == j || j < i) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == s[j])
-        {
-            return solve(i+1, j-1, s, dp);
-        }
-        int mini = 1e6;
+        if(i > j || i == j) return 0;
 
-        mini = min(mini, 1 + solve(i+1, j, s, dp));
-        mini = min(mini, 1 + solve(i, j-1, s, dp));
-        return dp[i][j] = mini;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == s[j]) return dp[i][j] = solve(i+1, j-1, s, dp);
+        else
+        {
+            int pos1 = solve(i+1, j, s, dp) + 1;
+            int pos2 = solve(i, j-1, s, dp) + 1;
+            return dp[i][j] = min(pos1, pos2);
+        } 
     }
     int minInsertions(string s) {
-        int  n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
         return solve(0, n-1, s, dp);
     }
 };
