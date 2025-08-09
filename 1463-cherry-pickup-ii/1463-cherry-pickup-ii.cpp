2d88de640e2c388fdf35854a5577a7658741b723
Avanish Shukla <avanishshukla2021@gmail.com>
2025-08-09 20:40:08 +0530

    Time: 98 ms (7.21%), Space: 19.5 MB (83.21%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,43 +1,51 @@
 class Solution {
 public:
-    int solve(int i, int j, int k, vector<vector<int>>& vec, vector<vector<vector<int>>>& dp)
-    {
-        if(j < 0 || k < 0 || j >= vec[0].size() || k >= vec[0].size())
-    return 0;
-        if(i >= vec.size())
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int> (n, 0)));
+
+        //base case for i = m - 1
+        for(int j1 = 0; j1 < n; j1++)
         {
-            return 0;
-        }
-
-        if(dp[i][j][k] != -1) return dp[i][j][k];
-
-        int sum = 0;
-        if(j == k) sum += vec[i][j];
-        else
-        {
-            sum += vec[i][j] + vec[i][k];
-        }
-
-        vector<int> p = { -1, 0, 1 };
-
-        int maxi = 0;
-        for(int step1 : p)
-        {
-            for(int step2 : p)
+            for(int j2 = 0; j2 < n; j2++)
             {
-                maxi = max(maxi, solve(i+1, j+step1, k+step2, vec, dp));
+                if(j1 == j2) dp[m-1][j1][j2] = grid[m-1][j1];
+                else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
             }
         }
 
-        maxi += sum;
-        return dp[i][j][k] = maxi;
-    }
-    int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid[0].size();
-        int m = grid.size();
 
-        vector<vector<vector<int>>> dp (m, vector<vector<int>>(n, vector<int>( n, -1)));
+        for(int row = m-2; row >= 0; row--)
+        {
+            for(int col1 = n-1; col1 >= 0; col1--)
+            {
+                for(int col2 = n-1; col2 >= 0; col2--)
+                {
+                    int maxcherry = -1e9;
+                    //explore paths
 
-        return solve(0, 0, n-1, grid, dp);
+                    for(int st1 = -1; st1 <= 1; st1++)
+                    {
+                        for(int st2 = -1; st2 <= 1; st2++)
+                        {
+                            int cherry = -1e9;
+
+                            if(st1 + col1 >= 0 && st1 + col1 < n && st2 + col2 >= 0 &&  st2 + col2 < n)  cherry = dp[row + 1][col1 + st1][col2+st2];
+
+                            if(col1 == col2) cherry += grid[row][col1];
+                            else cherry += grid[row][col1] + grid[row][col2];
+
+                            maxcherry = max(maxcherry, cherry);
+                        }
+                    }
+                    
+                    dp[row][col1][col2] = maxcherry;
+                }
+            }
+        }
+
+        return dp[0][0][n-1];
+
     }
 };
