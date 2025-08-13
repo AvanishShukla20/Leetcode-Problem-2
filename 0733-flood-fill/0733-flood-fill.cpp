9063072ff948f29545f23878bb33018254b4a806
Avanish Shukla <avanishshukla2021@gmail.com>
2025-08-13 14:48:32 +0530

    Time: 1 ms (21.77%), Space: 18.6 MB (9.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size(), n = image[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> grid = image;
 
-    void dfs(vector<vector<int>>& v,vector<vector<int>>& image,int start,int row,int col,int color)
-    {
-            v[row][col] = color;
-            vector<pair<int,int>> directions{{0,1},{0,-1},{1,0},{-1,0}};
-            int n = image.size();
-            int m = image[0].size();
-            for(int i = 0; i< directions.size(); i++)
-            {
-                int nr = row + directions[i].first;
-                int nc = col + directions[i].second;
-                if(nr >=0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == start && v[nr][nc] != color)
+        grid[sr][sc] = color;
+
+        q.push({sr, sc});
+         vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
+        vector<int> vis(m*n+1, 0);
+
+        vis[sr*n + sc + 1] = 1;
+
+        while(!q.empty())
+        {
+            auto r = q.front().first;
+            auto c = q.front().second;
+            q.pop();
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k]; 
+                int idx = nr*n + (nc+1);
+                if(nr >=0 && nr < m && nc >=0 && nc < n && !vis[idx] && image[nr][nc] == image[r][c])
                 {
-                    dfs(v, image, start, nr, nc,color);
+                    grid[nr][nc] = color;
+                    vis[idx] = 1;
+                    q.push({nr, nc});
                 }
             }
+        }
 
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int>> v(image.begin(), image.end());
-        int start = image[sr][sc];
-        dfs(v, image,start, sr, sc, color);
-        return v;
-
-
+        return grid;
     }
 };
