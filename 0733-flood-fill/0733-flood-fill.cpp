f750f2cf343da0d8157226fb52664ef1eda4a9f8
Avanish Shukla <avanishshukla2021@gmail.com>
2025-08-13 14:50:41 +0530

    Time: 0 ms (100%), Space: 18.1 MB (60.13%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,16 +3,17 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size(), n = image[0].size();
         queue<pair<int, int>> q;
-        vector<vector<int>> grid = image;
+        int StartPixelColor = image[sr][sc]; // you have to color all connected pixels with this color
 
-        grid[sr][sc] = color;
+        
+        if(StartPixelColor == color) return image;
+
+        image[sr][sc] = color;
 
         q.push({sr, sc});
-         vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, 1, 0, -1};
-        vector<int> vis(m*n+1, 0);
 
-        vis[sr*n + sc + 1] = 1;
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
 
         while(!q.empty())
         {
@@ -22,16 +23,14 @@ public:
             for (int k = 0; k < 4; k++) {
                 int nr = r + dr[k];
                 int nc = c + dc[k]; 
-                int idx = nr*n + (nc+1);
-                if(nr >=0 && nr < m && nc >=0 && nc < n && !vis[idx] && image[nr][nc] == image[r][c])
+                if(nr >=0 && nr < m && nc >=0 && nc < n  && image[nr][nc] ==  StartPixelColor)
                 {
-                    grid[nr][nc] = color;
-                    vis[idx] = 1;
+                    image[nr][nc] = color;
                     q.push({nr, nc});
                 }
             }
         }
 
-        return grid;
+        return image;
     }
 };
