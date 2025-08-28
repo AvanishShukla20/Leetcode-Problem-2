ef1475643c0fca501b8f2cbe929ba9dfeafac54a
Avanish Shukla <avanishshukla2021@gmail.com>
2025-08-28 17:56:15 +0530

    Time: 3 ms (88.85%), Space: 8.4 MB (92.41%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
+    int expand(const string& s, int left, int right) {
+        // Base case: out of bounds or characters don't match
+        if (left < 0 || right >= s.size() || s[left] != s[right]) return 0;
+        // Count the current palindrome, and expand further
+        return 1 + expand(s, left - 1, right + 1);
+    }
+
     int countSubstrings(string s) {
         int n = s.size();
-        int cnt = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-
-        for(int j=0;j<n;j++) // end is taken in first loop
-        {
-            for(int i=0; i<=j; i++)
-            {
-                if((s[i] == s[j]) && ( (j-i <= 1) || ( dp[i+1][j-1] == true)))
-                {
-                    dp[i][j] = true;
-                    cnt++;
-                }
-            }
-        } 
-
-        return cnt;
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            // Odd length palindromes (single center)
+            count += expand(s, i, i);
+            // Even length palindromes (center between i and i+1)
+            count += expand(s, i, i + 1);
+        }
+        return count;
     }
 };
