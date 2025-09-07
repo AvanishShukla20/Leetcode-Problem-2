7512c5b31f9425e9c101fedeeaa8f73fffd0bd1f
Avanish Shukla <avanishshukla2021@gmail.com>
2025-09-07 15:55:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (8.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    bool isMatch(char& ch, char &sc)
-    {
-        return ((ch == '(' && sc == ')') || (ch == '{' && sc == '}') || (ch == '[' && sc == ']')) ;
+    bool isgood(char ch, stack<char>& st) {
+        if (st.empty()) return false;
+
+        char top = st.top();
+        if ((ch == ')' && top != '(') ||
+            (ch == '}' && top != '{') ||
+            (ch == ']' && top != '['))
+            return false;
+
+        st.pop();
+        return true;
     }
+
     bool isValid(string s) {
         stack<char> st;
-        int n=s.size();
-
-        for(int i=0; i<n; i++)
-        { 
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
-            else
-            {
-                // ')'  or '}' or ']'
-                
-                if(st.empty()) return false;
-                char ch = st.top();
-
-                if(!isMatch(ch , s[i]))
-                {
-                    return false;
-                }
-                st.pop();
-            }
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+            if (isgood(s[i], st) == false) return false;
         }
-
-        return st.empty();
+        else if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            st.push(s[i]);
+        }
+    }
+    return st.empty();
     }
 };
