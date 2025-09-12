73e3ae956b4294d8e8efb3c90a6d4bb20b2bb1ed
Avanish Shukla <avanishshukla2021@gmail.com>
2025-09-12 19:57:17 +0530

    Time: 2 ms (34.34%), Space: 16.5 MB (97.42%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,39 +2,50 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        queue<pair<int, pair<int, int>>> q;
-        
+        queue<pair<int, int>> q;
+        int fresh = 0;
         for(int i=0; i<m; i++)
         {
             for(int j =0; j<n; j++)
             {
                 if(grid[i][j] == 2)
                 {
-                    // vis[i][j] = 1;
-                    if(grid[i][j] == 2) q.push({0, {i, j}});
+                   q.push({i, j});
                 }
-                
+                else if(grid[i][j] == 1) fresh++;
             }
         }
+
+        // if there are no fresh oranges then return 0
+        if(fresh == 0) return 0;
+
+        //if(q.empty()) return -1;
+
+
         int dr[] = {0, -1, 0, 1};
         int dc[] = {-1, 0, 1, 0};
         int ans = 0;
         while(!q.empty())
         {
-            auto t = q.front();
-            q.pop();
-            int lv = t.first, i = t.second.first, j = t.second.second;
-            ans = lv;
-            for(int k =0;k<4;k++)
+            int len = q.size();
+            ans++;
+            for(int k=0; k<len; k++)
             {
-                int ni = i+dr[k];
-                int nj = j+dc[k];
-                if(ni>=0&&ni<m&&nj>=0&&nj<n && grid[ni][nj] == 1)
+                auto t = q.front();
+                q.pop();
+                int i = t.first, j = t.second;
+                for(int k =0;k<4;k++)
                 {
-                    grid[ni][nj] = 2;  
-                    q.push({lv+1, {ni, nj}});
+                    int ni = i+dr[k];
+                    int nj = j+dc[k];
+                    if(ni>=0&&ni<m&&nj>=0&&nj<n && grid[ni][nj] == 1)
+                    {
+                        grid[ni][nj] = 2;  
+                        q.push({ni, nj});
+                    }
                 }
             }
+            
         }
 
         for(int i=0; i<m; i++)
@@ -44,6 +55,6 @@ public:
                 if(grid[i][j] == 1) return -1;
             }
         }
-        return ans;
+        return ans-1;
     }
 };
